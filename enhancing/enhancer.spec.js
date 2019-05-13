16ca1f3d98cb0af0b9965b0d2a151a11fe71ba0e
“Josefia <josefia.magagnoli@gmail.com>
2019-05-13 19:41:49 -0400

    Added describe methods

--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -2,20 +2,27 @@ const enhancer = require('./enhancer.js');
 // test away!
 
 // Test Repair
-it('repairs weapon\'s durabilty', () => {
-    expect(enhancer.repair(item)).toBe(item.durability === 100)
+describe('repair() method', () => {
+    it('repairs weapon\'s durabilty', () => {
+        expect(enhancer.repair(item)).toBe(item.durability === 100)
+    }) 
 })
+
 
 // Test Succeed
-it('if enhancement is less than 20, increase by 1', () => {
-    expect(enhancer.succeed(item)).toBe(item.enhancement++ || item.enhancement)
+describe('enhancementPassed', () => {
+    it('if enhancement is less than 20, increase by 1', () => {
+        expect(enhancer.succeed(item)).toBe(item.enhancement++ || item.enhancement)
+    })    
 })
+
 
 // Test Fail
-it ('enhancement fails', () => {
-    expect(enhancer.fail(item)).toBe((item.durability =- 5) || (item.durability =- 10 && item.enhancement--) || (item.durability =- 1) )
+describe('enhancementFailed', () => {
+    it ('enhancement fails', () => {
+        expect(enhancer.fail(item)).toBe((item.durability =- 5) || (item.durability =- 10 && item.enhancement--) || (item.durability =- 1) )
+    })    
 })
-
 
 const item = {
     durability: 72,
